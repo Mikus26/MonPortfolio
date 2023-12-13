e0c31e76757687ece86c3d9cc13287df3d11cb77
Mikus26 <m.walenciak@icloud.com>
2023-12-13 16:02:33 +0100

    First commit

---
+++ src/Entity/Commentaires.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentairesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentairesRepository::class)]
+class Commentaires
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $note = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $actif = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?User $commentaires = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getActif(): ?int
+    {
+        return $this->actif;
+    }
+
+    public function setActif(int $actif): static
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getCommentaires(): ?User
+    {
+        return $this->commentaires;
+    }
+
+    public function setCommentaires(?User $commentaires): static
+    {
+        $this->commentaires = $commentaires;
+
+        return $this;
+    }
+}

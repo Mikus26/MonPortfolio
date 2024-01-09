57e7287ace6a4f20d08c7cd0d2c38644be240e2e
Mikus26 <m.walenciak@icloud.com>
2024-01-09 20:46:31 +0100

    nab

---
+++ assets/base.js
@@ -0,0 +1,7 @@
+import "./styles/base.scss";
+import "./img/MW_2.png";
+import "./img/JSS.png";
+import "./img/adobexdd.png";
+import "./img/css33.png";
+import "./img/html55.png";
+import "./img/symf.png";
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('base', './assets/base.js')
+    .addEntry('nav','./assets/navbar.js')
+    
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -54,7 +57,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
